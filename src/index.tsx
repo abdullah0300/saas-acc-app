979f17765fe380449eabd8338cda1f9b17d55db2
Abdullah Aslam <aslamabdullah724@gmail.com>
2025-07-06 03:29:22 +0500

    done

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import SplashScreen from './SplashScreen';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-        <SplashScreen>
-
     <App />
-        </SplashScreen>
-
   </React.StrictMode>
 );
